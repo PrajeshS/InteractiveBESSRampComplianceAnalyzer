2940e7fd45b38bd0e59b40226d7a8d0df85afdac
PrajeshS <89408105+PrajeshS@users.noreply.github.com>
2026-06-24 09:55:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.sidebar.header("⚙️ Physical & Simulation Parameters")
 pwr_cap = st.sidebar.number_input('BESS Power Limit (MW)', 0, 4000, 20)
 enr_cap = st.sidebar.number_input('BESS Energy Capacity (MWh)', 0, 8000, 40)
 init_soc_pct = st.sidebar.number_input('Initial Year SOC (%)', 0, 100, 50)
-eff_one_way = st.sidebar.number_input('One-Way Efficiency', 0.80, 1.00, 0.96, step=0.01)
+eff_one_way = st.sidebar.number_input('One-Way Efficiency', 0.80, 1.00, 0.97, step=0.01)
 
 soc_choice = st.sidebar.selectbox('Operating SOC Window', ['0% - 100%', '10% - 90%', '20% - 80%', '30% - 70%'])
 soc_min, soc_max = [float(x.replace('%', '').strip())/100 for x in soc_choice.split('-')]
