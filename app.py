610b8f9cbe2ee6909860a281e70b46407f017bff
PrajeshS <89408105+PrajeshS@users.noreply.github.com>
2026-08-11 10:02:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -345,7 +345,7 @@ c9.metric('Inherent Curtailment', f'{a_curt_inh:,.0f} MWh')
 c10.metric('Ramp Curtailment', f'{a_curt_ramp:,.0f} MWh')
 c11.metric('Total Curtailment', f'{((a_curt_inh + a_curt_ramp)/a_solar*100):.2f}%')
 c12.metric('Daytime Minutes (> 0.5 MW)', f'{day_mins:,} minutes')
-c13.metric('Naturally Compliant Daytime Minutes', f'{no_bess_compliant_minutes:,} minutes')
+c13.metric('Naturally Compliant Minutes', f'{no_bess_compliant_minutes:,} minutes')
 st.markdown(
     '<div class="section-header">Daily Ideal BESS Net Energy Required for ±3 MW/min Ramp Compliance</div>',
     unsafe_allow_html=True
